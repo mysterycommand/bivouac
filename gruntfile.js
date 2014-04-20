66e942377a381536914b89069d5b62cf7ad1036c
Matt Hayes <matt@mysterycommand.com>
2014-04-20 16:45:25 -0700

    Add Gruntfile and tasks/options. Get 'jshint' task working.

---
+++ gruntfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function loadConfig(path) {
+    var glob = require('glob');
+
+    var config = {};
+    var key;
+
+    glob.sync('*', {cwd: path})
+        .forEach(function(option) {
+            key = option.replace(/\.js$/,'');
+            config[key] = require(path + option);
+        });
+
+    return config;
+}
+
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
+
+    grunt.initConfig(grunt.util._.extend({
+        config: {
+            app: 'app',
+            dist: 'dist',
+            temp: '.tmp'
+        }
+    }, loadConfig('./tasks/options/')));
+
+    grunt.registerTask('default', [
+        'jshint'
+    ]);
+
+};
---
+++ tasks/options/jshint.js
@@ -0,0 +1,12 @@
+module.exports = {
+    options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+    },
+    all: [
+        'Gruntfile.js',
+        '<%= config.app %>/scripts/{,*/}*.js',
+        '!<%= config.app %>/scripts/vendor/*',
+        'test/spec/{,*/}*.js'
+    ]
+};
